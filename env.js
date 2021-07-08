1bcf3bdd30274797fa8e54654dd99d1ecabad287
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2021-07-08 18:21:30 -0300

    Update env.js

--- env.js
+++ env.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 , file = fs.readFileSync(path.resolve(process.cwd(), '.env'), 'utf8')
 
 file.toString().split(/\n|\r|\r\n/).map(async (linha) => {
-	let valor = linha.match(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/)
+	const valor = linha.match(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/)
 	if (valor != null) {
 		let val = valor[2] || '',
 		end = val.length - 1
@@ -13,6 +13,6 @@ file.toString().split(/\n|\r|\r\n/).map(async (linha) => {
 })
 
 Object.keys(env).map(async (key) => {
-	let retorno = (!Object.prototype.hasOwnProperty.call(process.env, key)) ? process.env[key] = env[key] : false
+	const retorno = (!Object.prototype.hasOwnProperty.call(process.env, key)) ? process.env[key] = env[key] : false
 	if (!retorno) console.log('[ENV] Erro na importação de valores.')
 })
